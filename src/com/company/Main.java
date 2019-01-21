ee5cad653e7b159bb25cdcf8e17ebd6e425200fc
Nada Mahmoud <nadamahmoud@Nadas-MBP.T-mobile.com>
2019-01-21 15:52:00 -0500

    included all pathways

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,6 +13,7 @@ public class Main {
         boolean userInput2 = true;
         boolean runAgain = true;
         String answer;
+
         do {
             System.out.println("Hello, What would you like to do today?" + lineBreak + "Enter 1 to Check Balance"
                     + lineBreak + "Enter 2 to Withdraw" + lineBreak + "Enter 3 to Deposit" + lineBreak +"Enter 4 to Log Out");
@@ -21,6 +22,15 @@ public class Main {
 
            if (userInput == 1) {
                System.out.println(accountBalance);
+
+               System.out.println("Would you like to another transaction? Y or N");
+               answer = reader.next();
+               if (answer.equals("Y")){
+                   runAgain = true;
+               }else {
+                   runAgain = false;
+               }
+
            } else if (userInput == 2) {
                System.out.println("Enter the amount you would like to withdraw:");
                double withdrawalAmount = reader.nextDouble();
@@ -38,6 +48,14 @@ public class Main {
                System.out.println("Enter the amount you would like to deposit:");
                     double depositAmount = reader.nextDouble();
                     System.out.println(accountBalance + depositAmount);
+
+               System.out.println("Would you like to another transaction? Y or N");
+               answer = reader.next();
+               if (answer.equals("Y")){
+                   runAgain = true;
+               }else {
+                   runAgain = false;
+               }
 
            } else if (userInput ==4) {
                System.out.println ("Goodbye");
